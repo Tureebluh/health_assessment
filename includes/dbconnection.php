44348aab170aa379b597067c4025ac581d9e4578
Jarek Thomas <Jarek Thomas>
2016-05-26 10:15:57 -0700

    forgot dbconn again

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,13 +1,13 @@
 <?php
-//  define("DB_SERVER", "us-cdbr-iron-east-04.cleardb.net");
-//  define("DB_USER", "b9565f77379fad");
-//  define("DB_PASS", "7b88fb7b");
-//  define("DB_NAME", "heroku_774b72170d79475");
+  define("DB_SERVER", "us-cdbr-iron-east-04.cleardb.net");
+  define("DB_USER", "b9565f77379fad");
+  define("DB_PASS", "7b88fb7b");
+  define("DB_NAME", "heroku_774b72170d79475");
         
-    define("DB_SERVER", "localhost");
-    define("DB_USER", "health_assessment");
-    define("DB_PASS", "6bJwhvMvjtWCuhMt");
-    define("DB_NAME", "health_assessment");
+//    define("DB_SERVER", "localhost");
+//    define("DB_USER", "health_assessment");
+//    define("DB_PASS", "6bJwhvMvjtWCuhMt");
+//    define("DB_NAME", "health_assessment");
         
     $dbconn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 

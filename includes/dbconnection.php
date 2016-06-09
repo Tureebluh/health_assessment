dc3a4e97d0560e933f3f867cd7d5b5a2d347c6ca
Jarek Thomas <Jarek Thomas>
2016-06-09 12:21:41 -0700

    improved find_user_by_email and fixed bugs with the Create/Edit form

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,15 +1,15 @@
 <?php
-//  define("DB_SERVER", $_ENV["DB_SERVER"]);
-//  define("DB_USER", $_ENV["DB_USER"]);
-//  define("DB_PASS", $_ENV["DB_PASS"]);
-//  define("DB_NAME", $_ENV["DB_NAME"]);
+  define("DB_SERVER", $_ENV["DB_SERVER"]);
+  define("DB_USER", $_ENV["DB_USER"]);
+  define("DB_PASS", $_ENV["DB_PASS"]);
+  define("DB_NAME", $_ENV["DB_NAME"]);
 
 	//Local database setup purely for testing purposes.
   
-    define("DB_SERVER", "localhost");
-    define("DB_USER", "health_assessment");
-    define("DB_PASS", "6bJwhvMvjtWCuhMt");
-    define("DB_NAME", "health_assessment");
+//    define("DB_SERVER", "localhost");
+//    define("DB_USER", "health_assessment");
+//    define("DB_PASS", "6bJwhvMvjtWCuhMt");
+//    define("DB_NAME", "health_assessment");
         
     $dbconn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
